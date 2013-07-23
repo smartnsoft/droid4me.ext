a4a09ac0f4ebbe17732f006c57278805a31636ca
Édouard Mercier <register@edouardmercier.fr>
2013-07-23 11:59:45 +0000

    Makes the "TypefaceManager" more generic.
    git-svn-id: https://svn2.smartnsoft.com/trunk/droid4me.ext@12390 6b92fa64-2e8f-456e-80a3-45d64efa668e

--- src/com/smartnsoft/droid4me/ext/graphics/TypefaceManager.java
+++ src/com/smartnsoft/droid4me/ext/graphics/TypefaceManager.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
 
+import com.smartnsoft.droid4me.ext.graphics.TypefaceManager.Typefaceable;
+
 /**
  * A helper class for managing Android fonts.
  * 
@@ -15,13 +17,27 @@ import android.graphics.Typeface;
  * @param <T>
  *          a enumerated type that the developer needs to create in order to name and refer to typefaces
  */
-public class TypefaceManager<T extends Enum<?>>
+public class TypefaceManager<T extends Typefaceable>
 {
+
+  public static interface Typefaceable
+  {
+
+    Typeface fromNative();
+  }
 
   private final Map<T, Typeface> typefacesMap = new HashMap<T, Typeface>();
 
   public final Typeface getTypeface(AssetManager assetManager, T typefaces)
   {
+    {
+      // We first check whether the font is native
+      final Typeface typeface = typefaces.fromNative();
+      if (typeface != null)
+      {
+        return typeface;
+      }
+    }
     {
       final Typeface typeface = typefacesMap.get(typefaces);
       if (typeface != null)
@@ -30,8 +46,8 @@ public class TypefaceManager<T extends Enum<?>>
       }
     }
 
-    final String fontPath = getTypefacePath(typefaces);
-    final Typeface typeface = Typeface.createFromAsset(assetManager, fontPath);
+    final String fontPathPrefix = getAssetsFontFolderPathPrefix();
+    final Typeface typeface = Typeface.createFromAsset(assetManager, fontPathPrefix + typefaces.toString());
     typefacesMap.put(typefaces, typeface);
     return typeface;
   }
