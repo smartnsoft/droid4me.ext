1cefae3ac0af484e9a4a627ff7027cb859d4d2e0
Édouard Mercier <register@edouardmercier.fr>
2013-07-28 11:37:57 +0000

    Going one step further with BCEL.
    git-svn-id: https://svn2.smartnsoft.com/trunk/droid4me.ext@12408 6b92fa64-2e8f-456e-80a3-45d64efa668e

---
+++ test/com/smartnsoft/droid4me/ext/test/Simple.java
@@ -0,0 +1,24 @@
+package com.smartnsoft.droid4me.ext.test;
+
+public class Simple
+{
+
+  public void simple()
+  {
+    if ("b".equals(""))
+    {
+      System.out.println("yes");
+    }
+    System.out.println("out");
+  }
+
+  public static int logAndroid(String tag, String message)
+  {
+    return 0;
+  }
+
+  public static void logSysout(String tag, String message)
+  {
+  }
+
+}
