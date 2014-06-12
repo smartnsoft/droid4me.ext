75e40b1953d441d095458a758f4d38255f2b7b73
Édouard Mercier <register@edouardmercier.fr>
2014-06-12 19:36:30 +0000

    Cleaning up the code.
    git-svn-id: https://svn2.smartnsoft.com/trunk/droid4me.ext@13640 6b92fa64-2e8f-456e-80a3-45d64efa668e

--- src/com/smartnsoft/droid4me/ext/app/ConnectivityListener.java
+++ src/com/smartnsoft/droid4me/ext/app/ConnectivityListener.java
@@ -1,3 +1,18 @@
+/*
+ * (C) Copyright 2009-2014 Smart&Soft SAS (http://www.smartnsoft.com/) and contributors.
+ *
+ * The code hereby is the full property of Smart&Soft, SIREN 444 622 690.
+ * 34, boulevard des Italiens - 75009 - Paris - France
+ * contact@smartnsoft.com - 00 33 6 79 60 05 49
+ *
+ * You are not allowed to use the source code or the resulting binary code, nor to modify the source code, without prior permission of the owner.
+ * 
+ * This library is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * Contributors:
+ *     Smart&Soft - initial API and implementation
+ */
+
 package com.smartnsoft.droid4me.ext.app;
 
 import android.app.Activity;
