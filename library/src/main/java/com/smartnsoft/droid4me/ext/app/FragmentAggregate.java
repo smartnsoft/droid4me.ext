bf3cfc6f75f83e01f723508b946254d5c6f7d0e0
Ludovic Roland <ludovic@smartnsoft.com>
2018-06-28 11:11:22 +0200

    RED-149 Update the droid4me dependency + clean-up deprecated code

--- library/src/main/java/com/smartnsoft/droid4me/ext/app/FragmentAggregate.java
+++ library/src/main/java/com/smartnsoft/droid4me/ext/app/FragmentAggregate.java
@@ -24,8 +24,8 @@ package com.smartnsoft.droid4me.ext.app;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.Fragment;
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.Fragment.SavedState;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
@@ -52,52 +52,27 @@ public abstract class FragmentAggregate<SmartApplicationClass extends SmartAppli
 
   protected final static Logger log = LoggerFactory.getInstance(FragmentAggregate.class);
 
-  protected final Fragment fragment;
-
-  protected final android.support.v4.app.Fragment supportFragment;
+  protected final Fragment supportFragment;
 
   private final FragmentAnnotation fragmentAnnotation;
 
-  public FragmentAggregate(Object fragment, FragmentAnnotation fragmentAnnotation)
+  public FragmentAggregate(Fragment fragment, FragmentAnnotation fragmentAnnotation)
   {
     this.fragmentAnnotation = fragmentAnnotation;
-    if (fragment instanceof android.support.v4.app.Fragment == false)
-    {
-      this.fragment = (android.app.Fragment) fragment;
-      this.supportFragment = null;
-    }
-    else
-    {
-      this.fragment = null;
-      this.supportFragment = (android.support.v4.app.Fragment) fragment;
-    }
+    this.supportFragment = fragment;
   }
 
   @SuppressWarnings("unchecked")
   @SuppressLint("NewApi")
   public SmartApplicationClass getApplication()
   {
-    if (fragment != null)
-    {
-      return (SmartApplicationClass) fragment.getActivity().getApplication();
-    }
-    else
-    {
-      return (SmartApplicationClass) supportFragment.getActivity().getApplication();
-    }
+    return (SmartApplicationClass) supportFragment.getActivity().getApplication();
   }
 
   @SuppressWarnings("unchecked")
   public SmartActivityClass getActivity()
   {
-    if (fragment != null)
-    {
-      return (SmartActivityClass) fragment.getActivity();
-    }
-    else
-    {
-      return (SmartActivityClass) supportFragment.getActivity();
-    }
+    return (SmartActivityClass) supportFragment.getActivity();
   }
 
   /**
